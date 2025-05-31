605cf05337d4e0645cae47595245b0a32f4867b7
enescitak <enescitak@gmail.com>
2025-05-31 21:00:15 +0300

    feat: add odev3 - LIKE operators and pattern matching

---
+++ odev3.sql
@@ -0,0 +1,21 @@
+SELECT country
+FROM country
+WHERE country LIKE 'A%a'
+ORDER BY country;
+
+SELECT country
+FROM country
+WHERE LENGTH(country) >= 6 AND country LIKE '%n'
+ORDER BY country;
+
+SELECT title
+FROM film
+WHERE title ILIKE '%T%T%T%T%'
+ORDER BY title;
+
+SELECT *
+FROM film
+WHERE title LIKE 'C%' 
+  AND length > 90 
+  AND rental_rate = 2.99
+ORDER BY title; 
